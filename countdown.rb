165881fc950c8f9a3a91253906e16be5863483d0
Ryan Brady <ryanbrady369@gmail.com>
2018-10-15 22:08:44 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,11 +4,13 @@ def countdown(int)
   count = int
   while count > 0
     puts "#{count} SECOND(S)!"
+    countdown_with_sleep
     count -= 1
   end
   return "HAPPY NEW YEAR!"
 end
 
 
-def countdown_with_sleep(num = 5)
+def countdown_with_sleep(num)
+  sleep 1
 end
